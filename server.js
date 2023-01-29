40f7a8563badafc0dee7e6c9145beb530c42dd6c
Software-engineering-codes <yrgaming873@gmail.com>
2023-01-29 13:52:12 +0000

    promotion web

---
+++ server.js
@@ -0,0 +1,14 @@
+let name = alert(prompt("Whats your name "))
+let age = prompt("Whats your age ")
+if(age>0){
+    console.log("Welcome to my journey of coding website")
+    alert("Welcome to my journey of coding website")
+
+}
+
+let email = prompt("whats your Email ")
+if(email){
+    console.log("Your email has submited")
+    alert("Your email has submited")
+
+}
